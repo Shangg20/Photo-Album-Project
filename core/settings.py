ac1219cfab3604236ebaebdcaf15d706cf4320d3
Ann Trecia Balendo <anntrecia.balendo@evsu.edu.ph>
2026-05-24 23:30:33 +0800

    FInal changes

--- core/settings.py
+++ core/settings.py
@@ -103,14 +103,7 @@ USE_TZ = True
 # Static files configuration for WhiteNoise
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STORAGES = {
-    "default": {
-        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files configuration for Cloudinary
 MEDIA_URL = '/media/'
